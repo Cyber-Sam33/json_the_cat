8429757aa6cb937bbdab6e03faf572caccd52374
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-19 21:23:19 +0000

    FetchBreeder refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -11,27 +11,26 @@ const request = require('request');
 // console.log(typeof data);
 // console.log('First entry in the data array: ', data[0]['weight']);
 
-args = process.argv.slice(2);
-breed = args[0].slice(0, 3).toLowerCase();
-console.log(breed);
 
-request(`https://api.thecatapi.com/v1/breedsxxx/searc?q=${breed}`, function(error, response, body) {
-  if (error) {
-    return console.error('error:', error); // Print the error if one occurred
-  }
-  if (response.statusCode === 404) {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // if (response.statusCode === 404) {
+    //   const data = JSON.parse(body);
+    //   console.log("Status Error Message ", data.message);
+    // }
+
+    // if (body.length <= 2) {
+    //   console.log("Sorry, we don't have that breed.  Try another one please.");
+    // }
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the homepage.
     const data = JSON.parse(body);
-    console.log("Status Error Message ", data.message);
-  }
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the homepage.
-  if (body.length <= 2) {
-    console.log("Sorry, we don't have that breed.  Try another one please.");
-  }
-  console.log(body.length);
 
-  console.error('error:', error);
-});
+    callback(error, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
 
 
